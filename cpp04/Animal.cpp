c49e49a072ee13cb803209cc25d7dac114c52881
Halil Samet Kotu <hakotu@k1m03s07.42istanbul.com.tr>
2025-12-26 12:49:44 +0300

    Ex04 created.

--- cpp04/Animal.cpp
+++ cpp04/Animal.cpp
@@ -6,16 +6,25 @@
 /*   By: hakotu <hakotu@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 17:54:02 by hakotu            #+#    #+#             */
-/*   Updated: 2025/12/19 17:55:08 by hakotu           ###   ########.fr       */
+/*   Updated: 2025/12/26 12:47:34 by hakotu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 
-Animal::Animal(/* args */)
+Animal::Animal()
 {
+    std::cout << "Animal constroctor called" << std::endl;
 }
+
+Animal::Animal(const Animal &copy)
+{
+    
+}
+
 
 Animal::~Animal()
 {
+    std::cout << "Animal destructor called" << std::endl;
+    
 }
